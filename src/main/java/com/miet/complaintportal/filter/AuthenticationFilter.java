e745f619f7fd5169c33fb3b0a400632468f222bf
Dazed-04 <singhsuryansh150404@gmail.com>
2026-08-27 16:25:47 +0530

    fix: require authentication for welcome.jsp

--- src/main/java/com/miet/complaintportal/filter/AuthenticationFilter.java
+++ src/main/java/com/miet/complaintportal/filter/AuthenticationFilter.java
@@ -12,7 +12,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
-@WebFilter(urlPatterns = { "/complaints/*", "/agent/*", "/admin/*" })
+@WebFilter(urlPatterns = { "/complaints/*", "/agent/*", "/admin/*", "/welcome.jsp" })
 public class AuthenticationFilter implements Filter {
 
   @Override
